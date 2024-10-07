8d10cf5ae381a92c3a8773510d2e008ea51d3cc2
DamianMillan <128572600+DamianMillan@users.noreply.github.com>
2024-10-07 04:38:18 -0700

    Updates

---
+++ precache-manifest.c5b5b3c20ca9de452bd9bc078b1ae181.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5039dfa9c55bae78de1807bf663f9f1",
+    "url": "/DamianMillan/Spaceapps.git/index.html"
+  },
+  {
+    "revision": "58f96aec55172ab533fd",
+    "url": "/DamianMillan/Spaceapps.git/static/css/main.c66fc559.chunk.css"
+  },
+  {
+    "revision": "00fd579e3670e0bf8d3f",
+    "url": "/DamianMillan/Spaceapps.git/static/js/2.1934cc0b.chunk.js"
+  },
+  {
+    "revision": "50944ac833af085f88ddda39b257ffd3",
+    "url": "/DamianMillan/Spaceapps.git/static/js/2.1934cc0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85546a72b3551bc2dfd",
+    "url": "/DamianMillan/Spaceapps.git/static/js/3.f893e0ed.chunk.js"
+  },
+  {
+    "revision": "58f96aec55172ab533fd",
+    "url": "/DamianMillan/Spaceapps.git/static/js/main.5441f7f3.chunk.js"
+  },
+  {
+    "revision": "ceba6051789c395e760c",
+    "url": "/DamianMillan/Spaceapps.git/static/js/runtime-main.eeed432b.js"
+  }
+]);
